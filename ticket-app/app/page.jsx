69333de4fd38e30e31d3809301abf54412ffb80f
joaoLouceiro <jrlouceiro@live.com>
2023-10-25 19:48:47 +0100

    - Renaming (api) to api - Create GET in route.js - Call GET in page.jsx - Create TicketCards dynamically - Separate TicketCards by Category

--- ticket-app/app/page.jsx
+++ ticket-app/app/page.jsx
@@ -1,14 +1,45 @@
 import React from "react";
 import TicketCard from "./(components)/TicketCard";
 
-const Dashboard = () => {
+const getTickets = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/Tickets", {
+      cache: "no-store",
+    });
+    return res.json();
+  } catch (error) {
+    console.log("Failed to get tickets", error);
+  }
+};
+
+const Dashboard = async () => {
+  const { tickets } = await getTickets();
+
+  const uniqueCategories = [
+    ...new Set(tickets?.map(({ category }) => category)), //using Set like this will remove all the category duplicates
+  ];
+
   return (
     <div className="p-5">
-      <div className="lg:grid grid-cols-2 xl:grid-cols-4">
-        <TicketCard />
-        <TicketCard />
-        <TicketCard />
-        <TicketCard />
+      <div>
+        {tickets &&
+          uniqueCategories?.map((uniqueCat, index) => (
+            //Whenever we are creating elements with an iterator, we need to give it a unique key
+            <div key={index} className="mb-4">
+              <h2>{uniqueCat}</h2>
+              <div className="lg:grid grid-cols-2 xl:grid-cols-4">
+                {tickets
+                  .filter((ticket) => ticket.category === uniqueCat)
+                  .map((filteredTicket, _index) => (
+                    <TicketCard
+                      id={_index}
+                      key={_index}
+                      ticket={filteredTicket}
+                    />
+                  ))}
+              </div>
+            </div>
+          ))}
       </div>
     </div>
   );
